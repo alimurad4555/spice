b1a5054124ecafd6e7436d1c8acdb237a4bef536
alimurad4555 <alimurad4555gmail.com>
2024-05-27 00:07:34 -0700

    Spice

---
+++ main.js
@@ -0,0 +1,5 @@
+// spice 
+var Name = "\n\t ali \n\t";
+console.log(Name);
+var Name1 = Name.trim();
+console.log(Name1);
---
+++ main.ts
@@ -0,0 +1,8 @@
+// spice 
+
+let Name: string ="\n\t ali \n\t"
+console.log(Name);
+
+let Name1: string = Name.trim();
+console.log(Name1);
+
